f7ec18339f789c1ef8c5dc5d7b0fb310e10284e1
Sanjivani Jadhav <sanju010305@gmail.com.com>
2024-12-27 10:55:08 +0530

    added quote

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ const quotes = [
     "Don't stop when you're tired. Stop when you're done.",
     "You are capable of amazing things!",
     "Believe you can and you're halfway there.",
-    "The only limit to our realization of tomorrow is our doubts of today."
+    "The only limit to our realization of tomorrow is our doubts of today.",
+    "The best Way To Predict The future is to invent it ",
+    "Your Limitation- it's Only Your imagination",
+    "Push yourself because no one else is going to do it for you",
+    "Great thing never come from comfort zones.",
+    "Dream it.Wish it.Do it."
 ];
 
 const quoteElement= document.getElementById('quote');
